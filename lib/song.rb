ed3e14667bcd7ff8c47bc3a0778610bb30d0353f
Eric Chrobak <eric@ericchrobak.com>
2018-05-22 02:02:27 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  attr_accessor :song, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists
+
+  def initialize()
+    @@count += 1
+  end
+
+  def count
+    @@count
+  end
+
+  def genres
+    
+  end
+
+  def artists
+    
+  end
+
+  def genre_count
+    
+  end
+
+  def artist_count
+    
+  end
+end
